97d2d0ab32c93c06f97798cd6fcc577f4a9653a3
k.f.gamehacker <k.f.gamehacker@gmail.com>
2019-07-18 14:05:44 +0800

    add some effects

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -8,12 +8,16 @@ class BookShlef extends Component{
             <h2 className="bookshelf-title">Currently Reading</h2>
             <div className="bookshelf-books">
               <ol className="books-grid">
-                <li>
-                    <Book />
-                </li>
-                <li>
-
-                </li>
+                <li><Book /></li>
+                <li><Book /></li>
+                <li><Book /></li>
+                <li><Book /></li>
+              </ol>
+              <ol className="books-grid">
+                <li><Book /></li>
+                <li><Book /></li>
+                <li><Book /></li>
+                <li><Book /></li>
               </ol>
             </div>
           </div>
